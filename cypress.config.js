304a95432a8ef874d3da6d49da5025a0f2a241a1
Joao Andrade <joaovandrade30@gmail.com>
2026-03-25 13:20:10 -0300

    chore: configuração inicial do projeto com Cypress e Faker

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+
+    baseUrl: 'https://front.serverest.dev/',
+    env: {
+      apiUrl: 'https://serverest.dev'
+    }
+  },
+});
